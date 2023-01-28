80cb04efa8956807d8dec9d9feb593338c464757
qhtkdrma <kimwonkyu15@gmail.com>
2023-01-28 21:44:46 +0900

    jinja2 사용을 위한 라이브러리 추가 및 홈페이지 제작

---
+++ projects/quick_tutorial/step_one/intro/app3/app3.py
@@ -0,0 +1,26 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.view import view_config
+
+
+@view_config(
+    route_name='hello',
+    renderer="/templates/home.jinja2"
+)
+def home(request):
+    return {"greet": 'welcome', 'name': 'Tut Ankh Amun'}
+
+
+if __name__ == '__main__':
+    HOST = '0.0.0.0'
+    PORT = 8080
+
+    with Configurator() as config:
+        config.include('pyramid_jinja2')
+        config.add_static_view(name='static', path='static')
+        config.add_route('hello', '/welcome')
+        config.scan()
+        app = config.make_wsgi_app()
+    server = make_server(HOST, PORT, app)
+    print(f'Starting server at {HOST}:{PORT}')
+    server.serve_forever()
